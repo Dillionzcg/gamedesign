9e92a7f054f6b55320018acc36ba3b8aefbfaab5
赵承刚 <dillion_2022@qq.com>
2026-09-01 12:05:09 +0800

    添加了具体藏品

--- 游戏设计新/游戏设计新.cpp
+++ 游戏设计新/游戏设计新.cpp
@@ -135,16 +135,7 @@ private:
 		"绝望"
 	};
 };
-//藏品池
-vector<RuneManager> Pool1 = {
 
-};
-vector<RuneManager> Pool2 = {
-
-};
-vector<RuneManager> Pool3 = {
-
-};
 class SkillManage {
 public:
 	SkillManage(int num) :ID(num) {}
@@ -209,6 +200,48 @@ private:
 	string Type;//类型
 	bool IfGotten = false;//是否已获得
 	int BuffNum;//加成数值
+};
+//藏品池
+vector<shared_ptr<Object>> Pool1 = {//初级藏品
+	make_shared<Object>(1,"M","A","攻击力+10%",0.1),
+	make_shared<Object>(1,"M","D","防御力+20%",0.2),
+	make_shared<Object>(1,"M","H","生命上限+30%",0.3),
+	make_shared<Object>(1,"M","HE","每点治疗能量治疗量+5",5),
+	make_shared<Object>(1,"E","A","敌人攻击力-10%",-0.1),
+	make_shared<Object>(1,"E","D","敌人防御力-10%",-0.1),
+	make_shared<Object>(1,"E","H","敌人生命上限-10%",-0.1),
+	make_shared<Object>(1,"E","CR","敌人暴击率-5%",-5),
+};
+vector<shared_ptr<Object>> Pool2 = {//中级藏品
+	make_shared<Object>(2,"M","A","攻击力+20%",0.2),
+	make_shared<Object>(2,"M","D","防御力+30%",0.3),
+	make_shared<Object>(2,"M","H","生命上限+40%",0.4),
+	make_shared<Object>(2,"M","HE","每点治疗能量治疗量+10",10),
+	make_shared<Object>(2,"E","A","敌人攻击力-15%",-0.15),
+	make_shared<Object>(2,"E","D","敌人防御力-15%",-0.15),
+	make_shared<Object>(2,"E","H","敌人生命上限-15%",-0.15),
+	make_shared<Object>(2,"E","CR","敌人暴击率-10%",-10),
+
+	make_shared<Object>(2,"M","IE","我方初始能量+1",1),
+	make_shared<Object>(2,"M","IHE","我方初始治疗能量+1",1),
+	make_shared<Object>(2,"M","MHE","我方治疗能量上限+2",2),
+	make_shared<Object>(2,"M","SC","可选择技能+1",1),
+};
+vector<shared_ptr<Object>> Pool3 = {//高级藏品
+	make_shared<Object>(3,"M","A","攻击力+30%",0.3),
+	make_shared<Object>(3,"M","D","防御力+40%",0.4),
+	make_shared<Object>(3,"M","H","生命上限+50%",0.5),
+	make_shared<Object>(3,"M","HE","每点治疗能量治疗量+15",15),
+	make_shared<Object>(3,"E","A","敌人攻击力-20%",-0.2),
+	make_shared<Object>(3,"E","D","敌人防御力-20%",-0.2),
+	make_shared<Object>(3,"E","H","敌人生命上限-20%",-0.2),
+	make_shared<Object>(3,"E","CR","敌人暴击率-15%",-15),
+
+	make_shared<Object>(3,"M","IE","我方初始能量+2",2),
+	make_shared<Object>(3,"M","IHE","我方初始治疗能量+2",2),
+	make_shared<Object>(3,"M","EN","我方技能所需能量-1",-1),
+	make_shared<Object>(3,"E","EN","敌方技能所需能量+2",2),
+	make_shared<Object>(2,"M","SC","可选择技能+1",1),
 };
 class Enemy;
 //局内buff管理，每个对象只负责一个buff
